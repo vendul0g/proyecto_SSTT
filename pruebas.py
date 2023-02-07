19f1f6fcbb5d2f8f108e9953c8234df101a44223
Alvaro Navarro <a.navarromartinez1@um.es>
2023-02-07 15:28:17 +0100

    initial commit

---
+++ pruebas.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+#!/usr/bin/env python3
+
+import sys 
+
+def main():
+    txt = "I could eat bananas all day"
+    x = txt.partition("bananas")
+    print(x) 
+
+
+if __name__ == "__main__":
+    main()
